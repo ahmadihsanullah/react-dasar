2571454bce0831152d4684c98ec14bebde60c517
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2025-06-23 17:38:00 +0700

    setup react menggunakan vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+     <div className="flex justify-center bg-blue-600 min-h-screen items-center text-white text-2xl">
+      Hello world!
+     </div>
+  )
+}
+
+export default App
